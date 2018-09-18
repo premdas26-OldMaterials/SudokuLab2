c7b55b146323aef8f6476f9f9cac87a19ab11861
matth <matth@128.4.159.112>
2018-09-18 14:14:18 -0400

    Added method headers from youtube

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,5 +1,37 @@
 package pkgGame;
 
-public class Sudoku {
+import java.util.Arrays;
 
+import pkgHelper.LatinSquare;
+
+public class Sudoku extends LatinSquare{
+	
+	public Sudoku() {
+		super();
+	}
+	
+	public Sudoku(int[][] latinSquare) {
+		super(latinSquare);
+	}
+	
+	protected int[][] getPuzzle(){
+		return super.getLatinSquare();
+	}
+	
+	protected int[] getRegion(int iRegionNbr) {
+		return null;
+	}
+	
+	protected int[] getRegion(int col, int Row) {
+		return null;
+	}
+	
+	protected boolean isSudoku() {
+		return false;
+	}
+	
+	protected boolean isPartialSudoku() {
+		return false;
+	}
 }
+
