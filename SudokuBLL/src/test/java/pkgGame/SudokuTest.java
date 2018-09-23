7a7e6008314e8edbbd8a29e14c0065f375cb6cfb
prema <prema@128.4.93.101>
2018-09-23 17:35:21 -0400

    test

--- SudokuBLL/src/test/java/pkgGame/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -2,11 +2,20 @@ package pkgGame;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class SudokuTest {
 
 	@Test
-	
+	public void getRegion_test1() throws java.lang.Exception {
+		
+		int[][] sud = {{1,2,3,4,5,6,7,8,9},{2,3,4,5,6,7,8,9,1},{3,4,5,6,7,8,9,1,2},{4,5,6,7,8,9,1,2,3},{5,6,7,8,9,1,2,3,4},{6,7,8,9,1,2,3,4,5},{7,8,9,1,2,3,4,5,6},{8,9,1,2,3,4,5,6,7},{9,1,2,3,4,5,6,7,8}};
+		Sudoku s = new Sudoku(sud);
+		int[] expectedRegion = {3,4,5,6,7,8,9,1,2};
+		assertTrue(Arrays.equals(s.getRegion(2), expectedRegion));
+		
+	}
 	
 }
