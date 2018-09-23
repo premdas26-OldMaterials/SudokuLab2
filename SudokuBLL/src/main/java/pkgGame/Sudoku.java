073abfb22be43f1638bb090d573b22acdb3c5ee2
Jackson <Jackson@wifi-roaming-128-4-89-248.host.udel.edu>
2018-09-23 16:36:17 -0400

    formatting

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -4,35 +4,36 @@ import java.util.Arrays;
 
 import pkgHelper.LatinSquare;
 
-public class Sudoku extends LatinSquare{
-	
+public class Sudoku extends LatinSquare {
+
 	private int iSize;
 	private int iSqrtSize;
-	
+
 	public Sudoku(int iSize) throws java.lang.Exception {
 		super();
-		if (Math.sqrt(iSize) == (int)(Math.sqrt(iSize))) {
+		if (Math.sqrt(iSize) == (int) (Math.sqrt(iSize))) {
 			int iSqrtSize = (int) Math.sqrt(iSize);
-		}else throw new java.lang.Exception();
+		} else
+			throw new java.lang.Exception();
 	}
-	
+
 	public Sudoku(int[][] latinSquare) throws java.lang.Exception {
 		super(latinSquare);
 	}
-	
-	protected int[][] getPuzzle(){
+
+	protected int[][] getPuzzle() {
 		return super.getLatinSquare();
 	}
-	
+
 	protected int[] getRegion(int r) {
 		int[] reg = new int[super.getLatinSquare().length];
-		
+
 		int i = (r % iSqrtSize) * iSqrtSize;
 		int j = (r / iSqrtSize) * iSqrtSize;
 		int iMax = i + iSqrtSize;
 		int jMax = j + iSqrtSize;
 		int iCnt = 0;
-		
+
 		for (; j < jMax; j++) {
 			for (i = (r % iSqrtSize) * iSqrtSize; i < iMax; i++) {
 				System.out.println("J: " + j + "    " + "I: " + i);
@@ -41,22 +42,21 @@ public class Sudoku extends LatinSquare{
 		}
 		return reg;
 	}
-	
+
 	protected int[] getRegion(int col, int Row) {
-		int region = (col/iSqrtSize) + 3*(Row/iSqrtSize);
+		int region = (col / iSqrtSize) + 3 * (Row / iSqrtSize);
 		return getRegion(region);
 	}
-	
+
 	protected boolean isSudoku() {
 		return false;
 	}
-	
+
 	protected boolean isPartialSudoku() {
 		return false;
 	}
-	
+
 	public boolean isValidValue(int iCol, int iRow, int iValue) {
 		return false;
 	}
 }
-
