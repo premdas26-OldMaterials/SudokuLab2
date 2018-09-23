3ba3f000f79646337a3b3c771026ef673edabaff
prema <prema@128.4.93.101>
2018-09-23 17:39:38 -0400

    getRegion test

--- SudokuBLL/src/test/java/pkgGame/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -13,7 +13,7 @@ public class SudokuTest {
 		
 		int[][] sud = {{1,2,3,4,5,6,7,8,9},{2,3,4,5,6,7,8,9,1},{3,4,5,6,7,8,9,1,2},{4,5,6,7,8,9,1,2,3},{5,6,7,8,9,1,2,3,4},{6,7,8,9,1,2,3,4,5},{7,8,9,1,2,3,4,5,6},{8,9,1,2,3,4,5,6,7},{9,1,2,3,4,5,6,7,8}};
 		Sudoku s = new Sudoku(sud);
-		int[] expectedRegion = {3,4,5,6,7,8,9,1,2};
+		int[] expectedRegion = {7,8,9,8,9,1,9,1,2};
 		assertTrue(Arrays.equals(s.getRegion(2), expectedRegion));
 		
 	}
